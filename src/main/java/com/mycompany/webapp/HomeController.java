590aaf25d6ee86b0521e32aec091917817a84445
simbokyung98 <synk98@gmail.com>
2022-04-08 09:07:58 +0900

    설정 작업 [maven, log4j]

--- src/main/java/com/mycompany/webapp/HomeController.java
+++ src/main/java/com/mycompany/webapp/HomeController.java
@@ -25,6 +25,8 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("확인");
+		logger.debug("확인인");
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -34,6 +36,10 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	public void method() {
+		logger.info("확인");
 	}
 	
 }
